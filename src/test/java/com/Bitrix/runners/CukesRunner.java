457efa417d1afa0ff4e0246bdee62190c8c15209
MustafaM937 <64110230+MustafaM937@users.noreply.github.com>
2020-07-30 18:19:50 -0400

    committed

--- src/test/java/com/Bitrix/runners/CukesRunner.java
+++ src/test/java/com/Bitrix/runners/CukesRunner.java
@@ -1,4 +1,17 @@
 package com.Bitrix.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/Resources/Features",
+        glue = "com/Bitrix/step_definitions"
+
+
+)
 public class CukesRunner {
 }
--- src/test/java/com/Bitrix/step_definitions/Hooks.java
+++ src/test/java/com/Bitrix/step_definitions/Hooks.java
@@ -1,4 +1,22 @@
 package com.Bitrix.step_definitions;
 
+import com.Bitrix.utilities.ConfigurationReader;
+import com.Bitrix.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
 public class Hooks {
+
+    @Before
+    public void setUp(){
+        System.out.println("empty hook before");
+
+
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("empty hook after");
+
+    }
 }
