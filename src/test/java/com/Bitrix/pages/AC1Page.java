cf399b3216b290fbfdebbb7db8d391ff4d512fac
Mohammad-Ewaz <farhang.mohamad540@gmail.com>
2020-08-06 12:02:41 -0400

    added some codes

--- src/test/java/com/Bitrix/pages/AC1Page.java
+++ src/test/java/com/Bitrix/pages/AC1Page.java
@@ -6,11 +6,15 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AC1Page extends US1Page {
-    public AC1Page(){
+    public AC1Page() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
     @FindBy(css = "span#bx-b-uploadfile-blogPostForm")
     public WebElement uploadButton;
+
+    @FindBy(css = "[name='bxu_files[]']")
+    public WebElement uploadFileAndImages;
 
     @FindBy(xpath = "//table[@class='diskuf-selector-table wd-fa-add-file-light-table']/tbody/tr[1]/td[1]")
     public WebElement UploadFileButton;
@@ -41,8 +45,9 @@ public class AC1Page extends US1Page {
     @FindBy(xpath = "/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td/div/div[2]/div/div[1]/div/div/div[3]/div/div/form/div[1]/div/div[2]/div[4]/div[2]/table/tbody/tr[3]/td[3]/span/span/span[2]/a[2]/span[2]")
     public WebElement SpreadsheetLink;
 
-
     @FindBy(xpath = "/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td/div/div[2]/div/div[1]/div/div/div[3]/div/div/form/div[1]/div/div[2]/div[4]/div[2]/table/tbody/tr[3]/td[3]/span/span/span[2]/a[3]/span[2]")
     public WebElement PowePointLink;
 
+    @FindBy(id = "blog-submit-button-save")
+    public WebElement sendButton;
 }
